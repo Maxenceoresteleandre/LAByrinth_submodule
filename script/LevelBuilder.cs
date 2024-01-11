262f09fec90e1745fdb7317894882f64daac59c3
Maxence <78730104+Maxenceoresteleandre@users.noreply.github.com>
2024-01-11 11:37:05 +0100

    Update LevelBuilder.cs
    .

--- script/LevelBuilder.cs
+++ script/LevelBuilder.cs
@@ -8,17 +8,21 @@ public class LevelBuilder : MonoBehaviour
     public const bool SIZE_4x4 = true;
     public Vector3 min_4x4_pos;
     public Vector3 min_3x3_pos;
-    public double pillar_offset_x;
-    public double pillar_offset_z;
-    public float end_pos_z_4x4;
-    public float end_pos_z_3x3;
-    public float start_pos_z;
-    public float start_min_x_pos;
-    public float end_min_x_pos;
+    public float floor_height;
+    public double pillar_offset = 1.17;
+    // z c'est dans le sens sortie (petit) vers entrée (grand)
+    // x c'est marcher sur le côté
     
     public GameObject pillarPrefab;
     public GameObject startPlatform;
     public GameObject endPlatform;
+
+    void Start()
+    {
+        CreateLevelPillars(SIZE_4x4);
+        CreateStart(2, SIZE_4x4);
+        CreateEnd(1, SIZE_4x4);
+    }
 
     public void CreateLevelPillars(bool size) {
         Vector3 initialPos;
@@ -37,9 +41,21 @@ public class LevelBuilder : MonoBehaviour
         }
     }
 
-    public void CreateStart(int position_offset) {
-
+    public void CreateStart(int position_offset, bool size) {
+        float z_pos;
+        if (size == SIZE_3x3) z_pos = min_3x3_pos.z + 3 * pillar_offset;
+        else z_pos = min_4x4_pos.z + 4 * pillar_offset;
+        AddStartOrEnd(startPlatform, position_offset, z_pos)
     }
 
-    private void AddStartOrEnd(GameObject go, int pos_)
+    public void CreateEnd(int position_offset, bool size) {
+        float z_pos;
+        if (size == SIZE_3x3) z_pos = min_3x3_pos.z - pillar_offset;
+        else z_pos = min_4x4_pos.z - pillar_offset;
+        AddStartOrEnd(endPlatform, position_offset, z_pos)
+    }
+
+    private void AddStartOrEnd(GameObject go, int x_pos_offset, float z_position){
+        Instantiate(go, new Vector3(x_pos_offset * pillar_offset, floor_height, z_position), Quaternion.identity);
+    }
 }
