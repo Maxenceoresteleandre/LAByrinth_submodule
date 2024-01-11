0d9d30f02a29c18e2d4e282c41d3c9e86ef764a5
Maxence <78730104+Maxenceoresteleandre@users.noreply.github.com>
2024-01-11 11:54:06 +0100

    nhibugtvfrcdctfvygbuhnij,okjnhibugvyfctdxrsdctfvgybhnj

--- script/LevelBuilder.cs
+++ script/LevelBuilder.cs
@@ -4,12 +4,13 @@ using UnityEngine;
 
 public class LevelBuilder : MonoBehaviour
 {
+    public GameObject pillarsParent;
     public const bool SIZE_3x3 = false;
     public const bool SIZE_4x4 = true;
     public Vector3 min_4x4_pos;
     public Vector3 min_3x3_pos;
     public float floor_height;
-    public double pillar_offset = 1.17;
+    public float pillar_offset = 1.17f;
     // z c'est dans le sens sortie (petit) vers entrée (grand)
     // x c'est marcher sur le côté
     
@@ -36,7 +37,11 @@ public class LevelBuilder : MonoBehaviour
         }
         for (int i=0; i<nbOfPillars; i++) {
             for (int j=0; j<nbOfPillars; j++) {
-                Instantiate(pillarPrefab, initialPos + new Vector3(), Quaternion.identity);
+                GameObject newChild = Instantiate(pillarPrefab, new Vector3(), Quaternion.identity);
+                newChild.transform.parent = pillarsParent.transform;
+                newChild.transform.localScale = new Vector3(0.5f, 0.5f, 0.5f);
+                newChild.transform.position = initialPos + new Vector3(i*pillar_offset, 0f, j*pillar_offset);
+                Debug.Log("pillar["+i.ToString()+","+j.ToString()+"]: "+newChild.transform.position.ToString());
             }
         }
     }
@@ -45,17 +50,19 @@ public class LevelBuilder : MonoBehaviour
         float z_pos;
         if (size == SIZE_3x3) z_pos = min_3x3_pos.z + 3 * pillar_offset;
         else z_pos = min_4x4_pos.z + 4 * pillar_offset;
-        AddStartOrEnd(startPlatform, position_offset, z_pos)
+        AddStartOrEnd(startPlatform, position_offset, z_pos);
     }
 
     public void CreateEnd(int position_offset, bool size) {
         float z_pos;
         if (size == SIZE_3x3) z_pos = min_3x3_pos.z - pillar_offset;
         else z_pos = min_4x4_pos.z - pillar_offset;
-        AddStartOrEnd(endPlatform, position_offset, z_pos)
+        AddStartOrEnd(endPlatform, position_offset, z_pos);
     }
 
     private void AddStartOrEnd(GameObject go, int x_pos_offset, float z_position){
-        Instantiate(go, new Vector3(x_pos_offset * pillar_offset, floor_height, z_position), Quaternion.identity);
+        GameObject newChild = Instantiate(go, new Vector3(), Quaternion.identity);
+        newChild.transform.parent = pillarsParent.transform;
+        newChild.transform.position = new Vector3(x_pos_offset * pillar_offset, floor_height, z_position);
     }
 }
