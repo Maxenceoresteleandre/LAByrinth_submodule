17e7f40c397b1c732452900bf02bd3af8cadd05e
Jules Bouton <jules.bouton_popper@etu.sorbonne-universite.fr>
2024-01-15 18:35:36 +0100

    Grid added to generation

--- script/LevelBuilder.cs
+++ script/LevelBuilder.cs
@@ -7,21 +7,36 @@ public class LevelBuilder : MonoBehaviour
     public GameObject pillarsParent;
     public const bool SIZE_3x3 = false;
     public const bool SIZE_4x4 = true;
+
+    public int dim;
+    private bool levelSize = SIZE_3x3;
     public GameObject origin_4x4;
     public GameObject origin_3x3;
+    public GameObject origin_grid_4x4;
+    public GameObject origin_grid_3x3;
+
     private float pillar_offset = 1.17f;
     // z c'est dans le sens sortie (petit) vers entrée (grand)
     // x c'est marcher sur le côté
     
     public GameObject pillarPrefab;
+
+    public GameObject grid3x3Prefab;
+    public GameObject grid4x4Prefab;
     public GameObject startPlatform;
     public GameObject endPlatform;
+    public int startingX;
+    public int endingX;
 
     void Start()
     {
-        StartCoroutine(CreateLevelPillars(SIZE_4x4));
-        StartCoroutine(CreateStart(3, SIZE_4x4));
-        StartCoroutine(CreateEnd(0, SIZE_4x4));
+        if (dim == 3) {
+            levelSize = SIZE_3x3;
+        } else if (dim == 4) {
+            levelSize = SIZE_4x4;
+        }
+        StartCoroutine(CreateLevelPillars(levelSize));
+        CreateGrid(levelSize);
     }
 
     IEnumerator CreateLevelPillars(bool size) {
@@ -33,6 +48,26 @@ public class LevelBuilder : MonoBehaviour
         } else {
             ref_obj = origin_4x4;
             nbOfPillars = 4;
+        }
+        for (int i=-1; i<=nbOfPillars; i++) {
+            for (int j=0; j<=nbOfPillars; j++) {
+                if(i==nbOfPillars){
+                    if(j==startingX){
+                        GameObject newChild = Instantiate(startPlatform, new Vector3(), Quaternion.identity);
+                        newChild.transform.parent = pillarsParent.transform;
+                        newChild.transform.position = ref_obj.transform.position + new Vector3(i*pillar_offset, 0f, j*pillar_offset-0.5f*pillar_offset);
+                        newChild.transform.localScale = new Vector3(0.5f, 0.5f, 0.5f);
+                    }
+                }
+                else if(i==-1){
+                    if(j==endingX){
+                        GameObject newChild = Instantiate(endPlatform, new Vector3(), Quaternion.identity);
+                        newChild.transform.parent = pillarsParent.transform;
+                        newChild.transform.position = ref_obj.transform.position + new Vector3(i*pillar_offset, 0f, j*pillar_offset-0.5f*pillar_offset);
+                        newChild.transform.localScale = new Vector3(0.5f, 0.5f, 0.5f);
+                    }
+                }
+            }
         }
         for (int i=0; i<nbOfPillars; i++) {
             for (int j=0; j<nbOfPillars; j++) {
@@ -46,43 +81,27 @@ public class LevelBuilder : MonoBehaviour
         }
     }
 
-    IEnumerator CreateStart(int position_offset, bool size) {
-        Vector3 pos = new Vector3();
-        yield return new WaitForSeconds(1.0f);
+    void CreateGrid(bool size) {
+        GameObject ref_obj;
+        GameObject newChild;
         if (size == SIZE_3x3) {
-            Vector3 min_3x3_pos = origin_3x3.transform.position;
-            pos.z = min_3x3_pos.z + 4*pillar_offset;
-            pos.y = min_3x3_pos.y;
-            pos.x = min_3x3_pos.x + position_offset*pillar_offset;
+            ref_obj = origin_grid_3x3;
+            newChild = Instantiate(grid3x3Prefab, new Vector3(), Quaternion.identity);
+            newChild.GetComponent<GridLab>().endingY = 6;
+            
         } else {
-            Vector3 min_4x4_pos = origin_4x4.transform.position;
-            pos.z = min_4x4_pos.z + 4*pillar_offset;
-            pos.y = min_4x4_pos.y;
-            pos.x = min_4x4_pos.x + position_offset*pillar_offset;
+            ref_obj = origin_grid_4x4;
+            newChild = Instantiate(grid4x4Prefab, new Vector3(), Quaternion.identity);
+            newChild.GetComponent<GridLab>().endingY = 8;
         }
-        AddStartOrEnd(startPlatform, pos);
-    }
-
-    IEnumerator CreateEnd(int position_offset, bool size) {
-        Vector3 pos = new Vector3();
-        yield return new WaitForSeconds(1.0f);
-        if (size == SIZE_3x3) {
-            Vector3 min_3x3_pos = origin_3x3.transform.position;
-            pos.z = min_3x3_pos.z - pillar_offset;
-            pos.y = min_3x3_pos.y;
-            pos.x = min_3x3_pos.x + position_offset*pillar_offset;
-        } else {
-            Vector3 min_4x4_pos = origin_4x4.transform.position;
-            pos.z = min_4x4_pos.z - pillar_offset;
-            pos.y = min_4x4_pos.y;
-            pos.x = min_4x4_pos.x + position_offset*pillar_offset;
-        }
-        AddStartOrEnd(endPlatform, pos);
-    }
-
-    private void AddStartOrEnd(GameObject go, Vector3 pos){
-        GameObject newChild = Instantiate(go, new Vector3(), Quaternion.identity);
         newChild.transform.parent = pillarsParent.transform;
-        newChild.transform.position = pos;
+        newChild.transform.position = ref_obj.transform.position;
+        
+        newChild.GetComponent<GridLab>().startingX = startingX * 2;
+        newChild.GetComponent<GridLab>().endingX = endingX * 2;
+
+        newChild.GetComponent<GridLab>().startingY = 0;
+        
+        Debug.Log("grid created");
     }
 }
