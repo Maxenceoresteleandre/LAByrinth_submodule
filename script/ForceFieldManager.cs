76004b2d13a30861b4eafeb78ecd2c7aa45d1566
Maxence <78730104+Maxenceoresteleandre@users.noreply.github.com>
2024-01-16 22:04:43 +0100

    Update ForceFieldManager.cs
    .

--- script/ForceFieldManager.cs
+++ script/ForceFieldManager.cs
@@ -4,13 +4,15 @@ using UnityEngine;
 
 public class ForceFieldManager : MonoBehaviour
 {
-    public GameObject target;
+    public Vector3 targetPosition;
     public SecurityManager Sm=null;
     public List<Vector3> obstaclePositions = new List<Vector3>();
     private Vector3 doorPosition;
+    private GameObject player;
 
     void Start()
     {
+        player = GameObject.FindGameObjectWithTag("Player");
         GameObject door = GameObject.FindGameObjectWithTag("TangibleDoor");
         doorPosition = door.transform.position;
         GameObject secu = GameObject.FindGameObjectWithTag("ColumControl");
@@ -24,9 +26,26 @@ public class ForceFieldManager : MonoBehaviour
         }
     }
 
+    private Vector3 FindClosestObstacle()
+    {
+        float minDistance = Mathf.Infinity;
+        Vector3 playerPos = player.transform.position;
+        Vector3 closestObstacle = Vector3.zero;
+        for (int i = 0; i < obstaclePositions.Count; i++)
+        {
+            float distance = Vector3.Distance(obstaclePositions[i], playerPos);
+            if (distance < minDistance)
+            {
+                minDistance = distance;
+            }
+        }
+        return closestObstacle;
+    }
+
     // Update is called once per frame
     void Update()
     {
-        Sm.ChangeDefaultTrackedObjectPos(target.transform.position);
+        targetPosition = FindClosestObstacle();
+        Sm.ChangeDefaultTrackedObjectPos(targetPosition);
     }
 }
