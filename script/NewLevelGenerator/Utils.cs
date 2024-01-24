669f01f7f4cf6fb8c5cd5b55977c2abb77f76b0b
Jules Bouton <jules.bouton_popper@etu.sorbonne-universite.fr>
2024-01-24 02:03:16 +0100

    Convert whole level generation to NET2.0

---
+++ script/NewLevelGenerator/Utils.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class Utils : MonoBehaviour
+{
+    public static void ShuffleArray<T>(T[] array)
+    {
+        System.Random random = new System.Random();
+
+        for (int i = array.Length - 1; i > 0; i--)
+        {
+            // Generate a random index within the remaining unshuffled part of the array
+            int randomIndex = random.Next(0, i + 1);
+
+            // Swap the elements at randomIndex and i
+            (array[randomIndex], array[i]) = (array[i], array[randomIndex]);
+        }
+    }
+
+    public static int Sum(List<int> numbers)
+    {
+        int sum = 0;
+        foreach (int number in numbers)
+        {
+            sum += number;
+        }
+        return sum;
+    }
+
+    public static int Sum(int[] numbers)
+    {
+        int sum = 0;
+        foreach (int number in numbers)
+        {
+            sum += number;
+        }
+        return sum;
+    }
+}
+
