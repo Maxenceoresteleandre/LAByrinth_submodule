146370a482fe024980f74ea3477970bba53ff3bd
Maxence <78730104+Maxenceoresteleandre@users.noreply.github.com>
2024-01-26 15:30:46 +0100

    line resetter + line are working

--- script/LineResetter.cs
+++ script/LineResetter.cs
@@ -16,7 +16,7 @@ public class LineResetter : MonoBehaviour
     void Update()
     {
         // if player is too close to the start, we reset the line in levelGrid
-        if (Vector3.Distance(player.position, startPlatformCenter.transform.position) < 10f){
+        if (Vector3.Distance(player.position, startPlatformCenter.transform.position) < 1.5f){
             GridLab.ResetLine();
         }
     }
