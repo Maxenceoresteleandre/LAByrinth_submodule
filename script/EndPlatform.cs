ba78c7de362537f6ea8a50881a601cf2acd6a43a
Maxence <78730104+Maxenceoresteleandre@users.noreply.github.com>
2024-02-02 17:11:47 +0100

    added endCell

---
+++ script/EndPlatform.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndPlatform : MonoBehaviour
+{
+    private Transform player;
+    public GameObject endPlatformCenter;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = Camera.main.transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // if player is too close to the start, we reset the line in levelGrid
+        if (endPlatformCenter.GetComponent<Collider>().bounds.Contains(player.position)){
+            // GO TO NEXT LEVEL
+        }
+    }
+}
