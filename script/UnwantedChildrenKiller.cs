8145b3f3d612f94974e62df83fc561fc8a238162
Maxence <78730104+Maxenceoresteleandre@users.noreply.github.com>
2024-02-02 18:31:20 +0100

    added enw grround

--- script/UnwantedChildrenKiller.cs
+++ script/UnwantedChildrenKiller.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class UnwantedChildrenKiller : MonoBehaviour
 {
+    public GameObject bigGround;
     private List<string> tagsToKeep = new List<string> {"Keep", "Start", "End"};
     public IEnumerator SendUnwantedChildrenToSpace(){
         foreach (Transform child in transform)
@@ -14,6 +15,11 @@ public class UnwantedChildrenKiller : MonoBehaviour
             yield return new WaitForSeconds(0.2f);
             child.gameObject.AddComponent<GoDieInSpace>();
         }
+        InstantiateGround();
+    }
+
+    private void InstantiateGround(){
+        Instantiate(bigGround, new Vector3(0f, 0f, 0f), Quaternion.identity);
     }
 
     public bool IsTagToKeep(string tag){
